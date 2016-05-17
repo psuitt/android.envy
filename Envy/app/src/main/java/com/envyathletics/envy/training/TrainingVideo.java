6cecebe7e9857fc23b63966f1cde1cc4e27aee67
Sora <limitlessreverie@gmail.com>
2016-05-16 20:55:41 -0600

    New activities.

---
+++ Envy/app/src/main/java/com/envyathletics/envy/training/TrainingVideo.java
@@ -0,0 +1,13 @@
+package com.envyathletics.envy.training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrainingVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_video);
+    }
+}
